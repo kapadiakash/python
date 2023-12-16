d05b9b84af00e1e7701008db9f30db962d34888f
Akash kapadi <kapadiakash1@gmail.com>
2023-12-16 23:25:23 +0530

    function.py
    function code

---
+++ function.py
@@ -0,0 +1,38 @@
+# def isgreter(a,b):
+#     if a>b:
+#         print("First number is a greater")
+#     else:
+#         print("second number is greater")
+# isgreter(9,4)
+
+# y = 'akash'
+# def name(x):
+#     print("my name is :",x)
+# name(y)
+
+def multiply(x,y):
+    print('multiplication',x * y)
+
+def add(x,y):
+    print('Addition',x + y)
+
+def subtract(x,y):
+    print('subtract',x - y)
+    
+num1 = 2
+num2 = 5
+
+def calculate():
+    multiply(num1, num2)
+    add(num1,num2)
+    subtract(num1,num2)
+    
+calculate()
+
+
+
+
+# def name(fname,lname):
+#     print(fname,lname)
+#     return
+# name("akash","kapadi")
