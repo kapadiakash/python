96a52e5745972c15396a1cbd3f0dd0d33e08989b
Akash kapadi <kapadiakash1@gmail.com>
2023-12-23 21:47:00 +0530

    update

--- opps.py
+++ opps.py
@@ -1,7 +1,10 @@
-class akash():
-    def __init__(self) -> None:
-        print("Hii Hello")
-    def Car(s):
-        print("My name is akash")
-akash()
-akash().Car()
+# class akash():
+#     def __init__(self) -> None:
+#         print("Hii Hello")
+#     def Car(s):
+#         print("My name is akash")
+# akash()
+# akash().Car()
+
+
+
